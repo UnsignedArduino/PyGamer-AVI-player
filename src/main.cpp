bac3de0c799bb5ca1f2edafb8de97c01868997a6
Cyrus Yiu <UnsignedArduino@outlook.com>
2023-09-13 17:08:15 -0400

    Arcada start

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,31 @@
 #include <Arduino.h>
+// clang-format off
+// Needed because Adafruit_Arcade needs Adafruit_ImageReader which needs Adafruit_EPD
+#include <Adafruit_EPD.h>
+// Go to .pio\libdeps\adafruit_pygamer_m4\Adafruit Arcada Library\Boards\Adafruit_Arcada_PyGamer.h
+// (or your respective board) and comment out #define ARCADA_USE_JSON because it breaks compilation
+// Go to .pio\libdeps\adafruit_pygamer_m4\Adafruit GFX Library\Adafruit_SPITFT.h
+// and comment out #define USE_SPI_DMA because it does not work when compiled with PlatformIO
+// clang-format on
+#include <Adafruit_Arcada.h>
+
+Adafruit_Arcada arcada;
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
+
+  if (!arcada.arcadaBegin()) {
+    Serial.print("Failed to begin Arcada");
+    digitalWrite(LED_BUILTIN, HIGH);
+    while (true) {
+      ;
+    }
+  }
+
+  arcada.displayBegin();
+  arcada.setBacklight(255);
 }
 
-void loop() {
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-}
+void loop() {}
